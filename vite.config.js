d66bc82c7ae031990b3ed4152ec9166725acb168
simonbeirouti <hello@simonbeirouti.com>
2024-10-14 13:20:21 +0700

    fix dev issues preventing site to load

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,27 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
+import pages from 'vite-plugin-react-pages'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import path from "path";
 
 import {nodePolyfills} from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
+
 export default defineConfig({
   plugins: [
     react(),
-    nodePolyfills(),
+    pages({
+      pagesDir: 'src/pages',
+    }),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+      // Explicitly enable the process polyfill
+      include: ['process']
+    }),
     ViteImageOptimizer({
       // plugin options
       png: {
@@ -30,6 +43,36 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
+			process: 'process/browser',
+			stream: 'stream-browserify',
+			zlib: 'browserify-zlib',
+			util: 'util',
 		},
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			define: {
+				global: 'globalThis'
+			},
+			plugins: [
+				NodeGlobalsPolyfillPlugin({
+					process: true,
+					// buffer: true
+				}),
+				NodeModulesPolyfillPlugin()
+			]
+		}
+	},
+	build: {
+		rollupOptions: {
+			plugins: [
+				rollupNodePolyFill()
+			]
+		}
+	},
+	define: {
+		'global': 'globalThis',
+		'process.env': {},
+		'process.browser': true,
 	},
 });
