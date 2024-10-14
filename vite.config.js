e626422615e3e44cb73b217a21c8d3023cdcffcf
simonbeirouti <hello@simonbeirouti.com>
2024-10-14 10:33:43 +0700

    add tailwind & shadcn

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
 
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import {nodePolyfills} from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
-  base: 'https://near-examples.github.io/near-multichain/'
-})
+	plugins: [react(), nodePolyfills()],
+	base: "/",
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
