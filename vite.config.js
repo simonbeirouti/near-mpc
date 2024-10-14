10ada26c55ddb4f4ab311655fa46316ac08590c3
simonbeirouti <hello@simonbeirouti.com>
2024-10-14 12:28:39 +0700

    rmv bootstrap and add image optimisation pck

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,31 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import path from "path";
 
 import {nodePolyfills} from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), nodePolyfills()],
+  plugins: [
+    react(),
+    nodePolyfills(),
+    ViteImageOptimizer({
+      // plugin options
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 80,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 80,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 80,
+      },
+      // ... other image types
+    }),
+  ],
 	base: "/",
 	resolve: {
 		alias: {
