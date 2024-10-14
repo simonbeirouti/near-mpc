58c039fbbac863f829dd895e15676d1befb26b51
simonbeirouti <hello@simonbeirouti.com>
2024-10-14 10:40:19 +0700

    fix issue with path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 import {nodePolyfills} from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
